6aaf986a9586b1bdf5744d9fef3b940c4f88a551
huanglong <huanglong@163.com>
2022-11-22 13:57:58 +0800

    基础配置文件

---
+++ src/data/menuList.ts
@@ -0,0 +1,221 @@
+export declare interface Menu {
+    id: string,
+    icon?: string,
+    path?: string,
+    name?: string,
+    componentPath?: string,
+    title: string,
+    children?: Array<Menu>,
+}
+
+export const menuList1: Array<Menu> = [
+    {
+        id: "M0001",
+        icon: "HomeFilled",
+        path: "/home",
+        name: "DemoHome",
+        componentPath: "home.vue",
+        title: "系统首页",
+    },
+    {
+        id: "M0201",
+        icon: "Grid",
+        title: "表格相关",
+        children: [
+            {
+                id: "M0202",
+                path: "/table",
+                name: "DemoTable",
+                componentPath: "table.vue",
+                title: "基础表格",
+            },
+            {
+                id: "M0203",
+                path: "/table1",
+                name: "DemoTable1",
+                componentPath: "table1.vue",
+                title: "虚拟化表格",
+            },
+            {
+                id: "M0202",
+                path: "/table2",
+                name: "DemoTable2",
+                componentPath: "table2.vue",
+                title: "复杂表格",
+            }
+        ]
+    },
+    {
+        id: "M0003",
+        icon: "List",
+        path: "/tabs",
+        name: "DemoTabs",
+        componentPath: "tabs.vue",
+        title: "tab选项卡",
+    },
+    {
+        id: "M0004",
+        icon: "Tickets",
+        title: "表单相关",
+        children: [
+            {
+                id: "M0005",
+                path: "/form",
+                name: "DemoForm",
+                componentPath: "form.vue",
+                title: "基本表单",
+            },
+            {
+                id: "M0006",
+                path: "/editor",
+                name: "DemoEditor",
+                componentPath: "editor.vue",
+                title: "富文本编辑器",
+            },
+            {
+                id: "M0007",
+                title: "文件上传下载",
+                children: [
+                    {
+                        id: "M7001",
+                        path: "/upload",
+                        name: "DemoUpload",
+                        componentPath: "upload.vue",
+                        title: "文件上传",
+                    },
+                    {
+                        id: "M7002",
+                        path: "/download",
+                        name: "DemoDownload",
+                        componentPath: "download.vue",
+                        title: "文件下载",
+                    },
+                ],
+            },
+            {
+                id: "M0025",
+                path: "/print",
+                name: "DemoPrint",
+                componentPath: "print.vue",
+                title: "打印",
+            },
+        ],
+    },
+    {
+        id: "M0091",
+        icon: "SuitcaseLine",
+        path: "/MessageBox",
+        name: "DemoMessage",
+        componentPath: "message.vue",
+        title: "消息弹窗",
+    },
+    {
+        id: "M0009",
+        icon: "PictureRounded",
+        path: "/icon",
+        name: "DemoIcon",
+        componentPath: "icon.vue",
+        title: "自定义图标",
+    },
+    {
+        id: "M0010",
+        icon: "TrendCharts",
+        path: "/charts",
+        name: "DemoEharts",
+        componentPath: "charts.vue",
+        title: "echarts图表",
+    },
+    {
+        id: "M0020",
+        icon: "Share",
+        path: "/bpmn",
+        name: "DemoBpmn",
+        componentPath: "bpmn.vue",
+        title: "流程图",
+    },
+    {
+        id: "M0011",
+        icon: "Orange",
+        path: "/i18n",
+        name: "DemoI18n",
+        componentPath: "i18n.vue",
+        title: "国际化功能",
+    },
+    {
+        id: "M0016",
+        icon: "Setting",
+        path: "/env",
+        name: "DemoEnv",
+        componentPath: "env.vue",
+        title: "环境变量",
+    },
+    {
+        id: "M0012",
+        icon: "UserFilled",
+        title: "权限处理",
+        children: [
+            {
+                id: "M0013",
+                path: "/permission",
+                name: "DemoPermission",
+                componentPath: "permission.vue",
+                title: "权限测试",
+            },
+            {
+                id: "M0014",
+                path: "/404",
+                name: "DemoError404",
+                componentPath: "error404.vue",
+                title: "404页面",
+            },
+        ],
+    },
+    {
+        id: "M0015",
+        icon: "Avatar",
+        path: "/donate",
+        name: "DemoDonate",
+        componentPath: "donate.vue",
+        title: "支持作者",
+    },
+];
+
+export const menuList2 = [
+    {
+        id: "M0001",
+        icon: "HomeFilled",
+        path: "/home",
+        name: "DemoHome",
+        componentPath: "home.vue",
+        title: "系统首页",
+    },
+    {
+        id: "M0012",
+        icon: "UserFilled",
+        title: "权限处理",
+        children: [
+            {
+                id: "M0013",
+                path: "/permission",
+                name: "DemoPermission",
+                componentPath: "permission.vue",
+                title: "权限测试",
+            },
+            {
+                id: "M0014",
+                path: "/404",
+                name: "DemoError404",
+                componentPath: "error404.vue",
+                title: "404页面",
+            },
+        ],
+    },
+    {
+        id: "M0015",
+        icon: "Avatar",
+        path: "/donate",
+        name: "DemoDonate",
+        componentPath: "donate.vue",
+        title: "支持作者",
+    },
+];
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,18 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import VXETable from 'vxe-table'
+import 'vxe-table/lib/style.css'
+import { i18n } from './i18n'
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(Antd)
+app.use(VXETable)
+app.use(i18n)
 app.mount('#app')
